a7877f51455b6c6fdc6d5a8f7f3a0e695d0e5e60
florian schweizer <florian.schweizer@tum.de>
2021-03-19 20:20:49 +0100

    Initial Commit

---
+++ Sources/CommonUI/CommonUI.swift
@@ -0,0 +1,3 @@
+struct CommonUI {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import CommonUITests
+
+var tests = [XCTestCaseEntry]()
+tests += CommonUITests.allTests()
+XCTMain(tests)
