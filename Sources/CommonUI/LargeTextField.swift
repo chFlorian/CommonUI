d33f32e7b4bfdb880b0ddffb1ea83e96216e36ed
florian schweizer <florian.schweizer@tum.de>
2021-03-20 12:55:31 +0100

    Add LargeTextField

--- Sources/CommonUI/LargeTextField.swift
+++ Sources/CommonUI/LargeTextField.swift
@@ -8,7 +8,36 @@
 import SwiftUI
 
 public struct LargeTextField: View {
+    @Binding var text: String
+    @State var placeholder: String
+    
     public var body: some View {
-        Text("Hello World")
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(Color(.placeholderText))
+                    .opacity(0.75)
+                    .padding(8)
+                    .padding(.top, 3)
+            }
+            
+            TextEditor(text: $text)
+                .frame(height: 200)
+                .padding(3)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.lightGray))
+                )
+                .opacity(text.isEmpty ? 0.25 : 1)
+        }
+    }
+}
+
+struct LargeTextField_Preview: PreviewProvider {
+    @State static var text = ""
+    static var previews: some View {
+        LargeTextField(text: $text, placeholder: "Placeholder")
+            .previewDevice(.init(rawValue: "iPhone 12 Pro Max"))
+            .frame(width: 300, height: 300)
     }
 }
